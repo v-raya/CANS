9752e0ed484e7e98d06ae263ebb3e6593162122c
Nasar-cambria <nkprodware@gmail.com>
2019-01-14 13:05:51 -0800

    add non case worker scenario

---
+++ spec/regression/non_case_worker_scenarios_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require 'acceptance_helper'
+require 'feature'
+require 'page_objects/assessment_form'
+
+feature 'Non Case Worker Functionality' do
+  before(:all) do
+    @form = AssessmentForm.new
+  end
+
+  after(:all) do
+    logout
+  end
+
+  scenario 'Non Case worker login, creates assessment and logs out' do
+    login non_caseworker_json
+    expect(page).to have_content('To Start a CANS Assessment, Search and Select the Child')
+    @form.close_top_alert.click
+    expect(page).not_to have_content('To Start a CANS Assessment, Search and Select the Child')
+    @form.search_clients.click
+    @form.search_clients.set CLIENT_NAME
+    expect(@form.search_clients_link).to have_content('Case, Child 01 Test')
+    @form.search_clients_link.click
+    expect(page).to have_content 'Assessment History'
+  end
+end
